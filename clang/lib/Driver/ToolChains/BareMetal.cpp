df3757b544e5327a4458714d892939a41ac9dcfd
Artur Pietrek <pietrek@adacore.com>
2024-06-27 16:04:32 +0200

    Respect nolibc while linking for baremetal
    The option is interpreted as in GCC, i.e., it disables libc and closely
    related libraries but doesn't affect the startup files or the builtins.
    
    See merge request eng/toolchain/llvm-project!31.

--- clang/lib/Driver/ToolChains/BareMetal.cpp
+++ clang/lib/Driver/ToolChains/BareMetal.cpp
@@ -498,10 +498,13 @@ void baremetal::Linker::ConstructJob(Compilation &C, const JobAction &JA,
   if (TC.ShouldLinkCXXStdlib(Args))
     TC.AddCXXStdlibLibArgs(Args, CmdArgs);
 
-  if (!Args.hasArg(options::OPT_nostdlib, options::OPT_nodefaultlibs)) {
+  if (!Args.hasArg(options::OPT_nostdlib, options::OPT_nodefaultlibs,
+                   options::OPT_nolibc)) {
     CmdArgs.push_back("-lc");
     CmdArgs.push_back("-lm");
+  }
 
+  if (!Args.hasArg(options::OPT_nostdlib, options::OPT_nodefaultlibs)) {
     TC.AddLinkRuntimeLib(Args, CmdArgs);
   }
 
