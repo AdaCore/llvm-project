c3b2ea2aee86094025c8df628518a8e0ed366a27
Douglas B Rupp <rupp@adacore.com>
2025-03-17 15:43:32 -0700

    Don't use x18 on VxWorks
    Use x9 instead because x18 is the TCB pointer.
    Using x9 makes shadow stack checking unsupportable, so generate
    an error if this is attempted.
    
    Issue: eng/toolchain/llvm-project#28
    
    Merge request eng/toolchain/llvm-project!55

--- llvm/lib/Target/AArch64/AArch64Subtarget.h
+++ llvm/lib/Target/AArch64/AArch64Subtarget.h
@@ -262,6 +262,7 @@ public:
   bool isTargetAndroid() const { return TargetTriple.isAndroid(); }
   bool isTargetFuchsia() const { return TargetTriple.isOSFuchsia(); }
   bool isWindowsArm64EC() const { return TargetTriple.isWindowsArm64EC(); }
+  bool isTargetVxWorks7r2() const { return TargetTriple.isOSVxWorks7r2(); }
 
   bool isTargetCOFF() const { return TargetTriple.isOSBinFormatCOFF(); }
   bool isTargetELF() const { return TargetTriple.isOSBinFormatELF(); }
