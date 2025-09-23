aa496069040c13e43b77709f8dd28f838fd1bbdb
Tom Tromey <tromey@adacore.com>
2025-09-23 14:56:07 -0600

    Allow DW_OP_rot, DW_OP_neg, and DW_OP_abs in DIExpression
    The Ada front end can emit somewhat complicated DWARF expressions for
    the offset of a field.  While working in this area I found that I
    needed DW_OP_rot (to implement a branch-free computation -- it looked
    more difficult to add support for branching); and DW_OP_neg and
    DW_OP_abs (just basic functionality).

--- llvm/lib/IR/DebugInfoMetadata.cpp
+++ llvm/lib/IR/DebugInfoMetadata.cpp
@@ -1779,6 +1779,7 @@ bool DIExpression::isValid() const {
     case dwarf::DW_OP_bregx:
     case dwarf::DW_OP_push_object_address:
     case dwarf::DW_OP_over:
+    case dwarf::DW_OP_rot:
     case dwarf::DW_OP_consts:
     case dwarf::DW_OP_eq:
     case dwarf::DW_OP_ne:
@@ -1786,6 +1787,8 @@ bool DIExpression::isValid() const {
     case dwarf::DW_OP_ge:
     case dwarf::DW_OP_lt:
     case dwarf::DW_OP_le:
+    case dwarf::DW_OP_neg:
+    case dwarf::DW_OP_abs:
       break;
     }
   }
