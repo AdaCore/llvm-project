ad7f20e66a6691859441ff3bf51e4410e9e13f47
Tom Tromey <tromey@adacore.com>
2025-10-27 09:46:55 -0600

    Allow DIDerivedType as a bound in DISubrangeType
    Consider this Ada type:
    
       type Array_Type is array (Natural range <>) of Integer;
       type Record_Type (L1, L2 : Natural) is record
          I1 : Integer;
          A1 : Array_Type (1 .. L1);
          I2 : Integer;
          A2 : Array_Type (1 .. L2);
          I3 : Integer;
       end record;
    
    Here, the array fields have lengths that depend on the discriminants
    of the record type.  However, in this case the array lengths cannot be
    expressed as DWARF location expressions, with the issue being that
    "A2" has a non-constant offset, but an expression involving
    DW_OP_push_object_address will push the address of the field -- with
    no way to find the location of "L2".
    
    In a case like this, I believe the correct DWARF is to emit the array
    ranges using a direct reference to the discriminant, like:
    
     <3><1156>: Abbrev Number: 1 (DW_TAG_member)
        <1157>   DW_AT_name        : l1
    ...
     <3><1177>: Abbrev Number: 6 (DW_TAG_array_type)
        <1178>   DW_AT_name        : (indirect string, offset: 0x1a0b): vla__record_type__T4b
        <117c>   DW_AT_type        : <0x1287>
        <1180>   DW_AT_sibling     : <0x118e>
     <4><1184>: Abbrev Number: 7 (DW_TAG_subrange_type)
        <1185>   DW_AT_type        : <0x1280>
        <1189>   DW_AT_upper_bound : <0x1156>
    
    (FWIW this is what GCC has done for years.)
    
    This patch makes this possible in LLVM, by letting a DISubrangeType
    refer to a DIDerivedType.  gnat-llvm can then arrange for the DIE
    reference to be correct by arranging for the array type's scope to be
    the record.

--- llvm/lib/IR/DebugInfoMetadata.cpp
+++ llvm/lib/IR/DebugInfoMetadata.cpp
@@ -850,7 +850,7 @@ DISubrangeType::convertRawToBound(Metadata *IN) const {
     return BoundType();
 
   assert(isa<ConstantAsMetadata>(IN) || isa<DIVariable>(IN) ||
-         isa<DIExpression>(IN));
+         isa<DIExpression>(IN) || isa<DIDerivedType>(IN));
 
   if (auto *MD = dyn_cast<ConstantAsMetadata>(IN))
     return BoundType(cast<ConstantInt>(MD->getValue()));
@@ -860,6 +860,9 @@ DISubrangeType::convertRawToBound(Metadata *IN) const {
 
   if (auto *MD = dyn_cast<DIExpression>(IN))
     return BoundType(MD);
+
+  if (auto *DT = dyn_cast<DIDerivedType>(IN))
+    return BoundType(DT);
 
   return BoundType();
 }
